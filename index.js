0a3045d5190c6dcdbd51e01d0e1c9eac07dfabf4
allanstar-byte <allanmutisya263@gmail.com>
2022-06-04 16:43:17 +0300

    dodge

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+// Changing the dodger color
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    dodger.style.left = "0px";
+}
+
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 4}px`;
+    }
+}
+
+
+// const dodger = document.getElementById("dodger");
+
+// dodger.style.backgroundColor("#FF69B4");
+
+// dodger.style.bottom = "100px";
